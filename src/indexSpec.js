d8895e66ab96517fb968f1677481e44daa2cc536
lamtranweb <lamtran@gmail.com>
2017-01-03 22:27:15 -0800

    fix(tests): fix test

--- src/indexSpec.js
+++ src/indexSpec.js
@@ -3,7 +3,7 @@ var lib = require('./index');
 describe('color utils', function() {
   it('should work', function() {
     expect(lib.black).toEqual('#000');
-    expect(lib.white).toEqual('#FFX');
+    expect(lib.white).toEqual('#FFF');
     expect(lib.getRed('#F00')).toEqual(255);
   })
 })
