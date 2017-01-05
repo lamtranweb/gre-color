53c31f9e217305d24f43a9087294381b7605a88e
lamtranweb <lamtran@gmail.com>
2017-01-05 00:01:29 -0800

    chore(testing): more set ups for releasing and testing

---
+++ src/index.test.js
@@ -0,0 +1,9 @@
+var lib = require('./index');
+var expect = require('chai').expect;
+
+describe('color utils', function() {
+  it('should work!', function() {
+    expect(lib.black).to.equal('#000');
+    expect(lib.white).to.equal('#FFF');
+  })
+})
--- src/indexSpec.js
+++
@@ -1,9 +0,0 @@
-var lib = require('./index');
-
-describe('color utils', function() {
-  it('should work', function() {
-    expect(lib.black).toEqual('#000');
-    expect(lib.white).toEqual('#FFF');
-    expect(lib.getRed('#F00')).toEqual(255);
-  })
-})
